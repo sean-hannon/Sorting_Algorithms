b36041c237c0029caa34dc0f72733d1a1df63f23
sean-hannon <sean.w.hannon@gmail.com>
2018-02-04 16:13:03 +0000

    Updating main to print arrays on one line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         QuickSort quickSort = new QuickSort();
 //        int[] input = {54,12,35};
         int[] input = {24,2,45,20,56,75,2,56,99,53,12};
+        System.out.println("Inuput Array");
+        System.out.println(Arrays.toString(input));
         int[] quickSorted = quickSort.sort(input);
         System.out.println("Quick Sort");
-        for (int i = 0; i < quickSorted.length; i++) {
-            System.out.println(quickSorted[i]);
-        }
+        System.out.println(Arrays.toString(quickSorted));
+
         System.out.println("Bubble Sort");
         BubbleSort bubbleSort = new BubbleSort();
         int[] bubbleSorted = bubbleSort.sort(input);
-        for (int i = 0; i < bubbleSorted.length; i++) {
-            System.out.println(bubbleSorted[i]);
-        }
+        System.out.println(Arrays.toString(bubbleSorted));
+
         System.out.println("Selection Sort");
         SelectionSort selectionSort = new SelectionSort();
         int[] selectionSorted = selectionSort.sort(input);
-        for (int i = 0; i < selectionSorted.length; i++) {
-            System.out.println(selectionSorted[i]);
-        }
+        System.out.println(Arrays.toString(selectionSorted));
     }
 }
